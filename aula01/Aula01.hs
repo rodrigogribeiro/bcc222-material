598e2a465e386b33803acab2ccfda4eade9d7b8b
Rodrigo Ribeiro <rodrigogribeiro@gmail.com>
2022-03-01 17:22:58 -0300

    Updating the lecture code

--- aula01/Aula01.hs
+++ aula01/Aula01.hs
@@ -3,3 +3,16 @@ module Aula01 where
 sumUpTo :: Int -> Int
 sumUpTo 0 = 0
 sumUpTo n = n + sumUpTo (n - 1)
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+fact :: Int -> Int
+fact n = n * factorial (n - 1)
+fact 0 = 1
+
+greet :: String -> String
+greet name = "Hello, " ++ name ++ "!"
+
+hello name = "Hello, " ++ name ++ "!"
